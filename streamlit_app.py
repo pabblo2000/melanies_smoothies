8cfef5005c38d588b6ac9c68564fa2c7be3f679f
Pablo Álvaro <palvaroh2000@gmail.com>
2025-04-10 20:38:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,11 +6,7 @@ import pandas as pd
 
 # Write directly to the app
 st.title("🥤Customize Your Smoothie!🥤")
-st.write(
-    """
-    Choose the fruits you want in your custom Smoothie!
-    """
-)
+st.write("""Choose the fruits you want in your custom Smoothie!""")
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
@@ -23,8 +19,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+s#t.stop()
 
 
 ingredients_list = st.multiselect(
@@ -38,8 +34,12 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
         st.subheader(fruit_chosen + 'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ fruit_chosen)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ search_on)
         sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
         
     # st.write(ingredients_string)
